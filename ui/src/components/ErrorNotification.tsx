f1d21f00831f0adfe87c7517cdcc50133f0913c9
John Rillos <johnrillos@gmail.com>
2022-09-08 17:39:44 -0500

    UI: error notification

---
+++ ui/src/components/ErrorNotification.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+
+export default function ErrorNotification(): JSX.Element | null {
+  const { errorMessage, dismissError } = useContext(AppContext);
+  if (!errorMessage) {
+    return null;
+  }
+  return (
+    <div className='fixed bottom-2 left-2 z-20'>
+      Error: {errorMessage}
+    </div>
+  );
+}
