174b07670f55918e4f220535c489fc5cd8b095bd
John Rillos <johnrillos@gmail.com>
2022-10-17 20:58:45 -0500

    bugfix: moons and comets don't crash UI

--- ui/src/components/Sigil.tsx
+++ ui/src/components/Sigil.tsx
@@ -1,6 +1,9 @@
 import { sigil, reactRenderer } from '@tlon/sigil-js'
 
-export default function Sigil(props: { ship: string }) {
+export default function Sigil(props: { ship: string }): JSX.Element | null {
+  if (props.ship.split('-').length > 2) {
+    return null;
+  }
   return sigil({
     class: 'rounded',
     patp: props.ship,
